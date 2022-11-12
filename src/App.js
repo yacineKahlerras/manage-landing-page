6ea7279e6f384c7b4562456c8a263c5481780a23
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2022-11-13 00:59:22 +0100

    ok

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ export default function App() {
       <main>
         <Hero />
         <Functionality />
-        <Reviews />
       </main>
+      <Reviews />
       <Footer />
     </>
   );
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,22 @@
 import React from "react";
 
 export default function Hero() {
-  return <div></div>;
+  return (
+    <div className="hero">
+      <TextSide />
+    </div>
+  );
+}
+
+function TextSide() {
+  return (
+    <div className="text-side">
+      <h1>Bring everyone together to build better products.</h1>
+      <p>
+        Manage makes it simple for software teams to plan day-to-day tasks while
+        keeping the larger team goals in view.
+      </p>
+      <button className="get-started-btn">Get Started</button>
+    </div>
+  );
 }
