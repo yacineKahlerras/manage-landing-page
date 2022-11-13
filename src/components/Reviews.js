3c1467049fd2f21e54495b3329b350c886b7f50d
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2022-11-13 13:39:45 +0100

    reviews

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,5 +1,61 @@
 import React from "react";
+import aliImg from "../images/avatar-ali.png";
+import anishaImg from "../images/avatar-anisha.png";
+import richardImg from "../images/avatar-richard.png";
+import shanaiImg from "../images/avatar-shanai.png";
+
+const reviewsContent = [
+  {
+    img: anishaImg,
+    name: "Anisha Li",
+    comment: `
+    “Manage has supercharged our team’s workflow. The ability to maintain 
+     visibility on larger milestones at all times keeps everyone motivated.”
+    `,
+  },
+  {
+    img: aliImg,
+    name: "Ali Bravo",
+    comment: `
+    “We have been able to cancel so many other subscriptions since using 
+    Manage. There is no more cross-channel confusion and everyone is much 
+    more focused.”
+    `,
+  },
+  {
+    img: richardImg,
+    name: "Richard Watts",
+    comment: `
+    “Manage allows us to provide structure and process. It keeps us organized 
+    and focused. I can’t stop recommending them to everyone I talk to!”
+    `,
+  },
+  {
+    img: shanaiImg,
+    name: "Shanai Gough",
+    comment: `
+    “Their software allows us to track, manage and collaborate on our projects 
+    from anywhere. It keeps the whole team in-sync without being intrusive.”
+    `,
+  },
+];
 
 export default function Reviews() {
-  return <div></div>;
+  const reviewsElements = reviewsContent.map((r) => {
+    return (
+      <div className="review">
+        <img src={r.img} alt={r.name} />
+        <h3>{r.name}</h3>
+        <p>{r.comment}</p>
+      </div>
+    );
+  });
+
+  return (
+    <div className="reviews">
+      <h2>What they’ve said</h2>
+      <div className="reviews-container">{reviewsElements}</div>
+      <button className="get-started-btn">Get Started</button>
+    </div>
+  );
 }
