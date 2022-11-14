5824c88a6982021d19d7abf788350f28a24a429b
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2022-11-14 20:07:21 +0100

    sidemenu tweaks

--- src/components/Sidemenu.js
+++ src/components/Sidemenu.js
@@ -1,14 +1,28 @@
 import React from "react";
 
 export default function SideMenu({ activeSubmenu, setActiveSubmenu }) {
+  // exists the submenu
+  function exitMenu() {
+    setActiveSubmenu(false);
+  }
+
+  // when we click the dark background we exit sidemenu
+  function backgroundClickExit(event) {
+    if (!event.target.classList.contains("sidemenu")) return;
+    exitMenu();
+  }
+
   return (
-    <aside className={`sidemenu ${activeSubmenu ? "sidemenu-active" : ""}`}>
+    <aside
+      onClick={backgroundClickExit}
+      className={`sidemenu ${activeSubmenu ? "sidemenu-active" : ""}`}
+    >
       <ul className="sidemenu-center">
-        <li>Pricing</li>
-        <li>Product</li>
-        <li>About us</li>
-        <li>Careers</li>
-        <li>Community</li>
+        <li onClick={exitMenu}>Pricing</li>
+        <li onClick={exitMenu}>Product</li>
+        <li onClick={exitMenu}>About us</li>
+        <li onClick={exitMenu}>Careers</li>
+        <li onClick={exitMenu}>Community</li>
       </ul>
     </aside>
   );
