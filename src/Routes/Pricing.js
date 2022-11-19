521601f875d65dd499b57837a690b19ba6ba19bb
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2022-11-19 13:09:58 +0100

    routing pricing page

--- src/Routes/Pricing.js
+++ src/Routes/Pricing.js
@@ -1,9 +1,11 @@
 import React from "react";
+import pricingImg from "../images/routes/pricing.svg";
 
 export default function Pricing() {
   return (
-    <>
+    <div className="route-page">
       <h1>Pricing</h1>
-    </>
+      <img src={pricingImg} alt="pricing img" />
+    </div>
   );
 }
